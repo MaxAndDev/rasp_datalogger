edc19960d21b635eaae3b36301f690476d8aa36f
gopaul96 <32932922+gopaul96@users.noreply.github.com>
2019-10-26 09:37:47 +0200

    Create sensehat.py

---
+++ sensehat.py
@@ -0,0 +1,30 @@
+from sense_hat import SenseHat
+station_id=1
+sense = SenseHat()
+sense.clear()
+pressure = sense.get_pressure()
+temp = sense.get_temperature()
+humidity = sense.get_humidity()
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+
+output = {
+    "station_id": station_id,
+    "airpressure": pressure,
+    "humidity": humidity,
+    "temperature": temp,
+    "timestamp": now,
+    "latitude": 50.9271,
+    "longitude": 11.5892,
+    "position_tag": "kitchen"
+  }
+  
+print(output)
+import json
+outputJson = json.dumps(output)
+
+print(type(outputJson))
+
+# for HTTP Post: https://www.geeksforgeeks.org/get-post-requests-using-python/
